a5c6ec133d2f933522ec70dee4fb699d42624e37
ssoi <sameer@betterpath.com>
2015-04-25 16:17:43 -0400

    fix(build): add latest version of scalatest
    feat(problem-01): use tail recursive solution
    
    fix(problem-01): add annotation library
    
    feat(problem-01): recursive solution to problem 1

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ lazy val root = (project in file(".")).
     version := "1.0",
     scalaVersion := "2.11.4"
   )
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/main/scala/scala-99-problems.scala
+++ src/main/scala/scala-99-problems.scala
@@ -1,12 +1,12 @@
 package com.sameersoi.scala99problems
 
+import scala.annotation.tailrec
+
 object ListSolutions {
   def last[A](input: List[A]): A = {
-    input.length match {
-      case 1 =>
-        input.head
-      case _ =>
-        last(input.tail)
+    input.tail.isEmpty match {
+      case true => input.head
+      case _ => last(input.tail)
     }
   }
 }
